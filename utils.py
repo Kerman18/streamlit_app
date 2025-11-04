90b76f259da1b7572ef97eb8580159d2c51ceae0
Kerman <122251729+Kerman18@users.noreply.github.com>
2025-11-04 23:19:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -31,38 +31,35 @@ def clean_text(text):
     return text
 
 @torch.no_grad()
-def generate_next_words(model, word2idx, idx2word, text, act_fun,num_words, temperature, context_length,top_k):
+def generate_next_words(model, word2idx, idx2word, text, act_fun, num_words, temperature=1.0, context_length=20, top_k=10):
     model.eval()
     cleaned_text = clean_text(text)
     words = cleaned_text.split()
 
-    # Convert tokens to indices (handle unknowns)
     unk_idx = word2idx["<unk>"]
     pad_idx = word2idx[" <pad> "]
 
-    token = [word2idx.get((' '+w), unk_idx) for w in words]
-    context=token[-context_length:]
+    token = [word2idx.get(' '+w, unk_idx) for w in words]
+    context = token[-context_length:]
 
     if len(context) < context_length:
-            context = [pad_idx] * (context_length - len(context)) + context
+        context = [pad_idx] * (context_length - len(context)) + context
 
     for _ in range(num_words):
         x = torch.tensor(context, dtype=torch.long).unsqueeze(0)
-        logits = model(x,act_fun)
+        logits = model(x, act_fun)
 
         probs = F.softmax(logits / temperature, dim=1)
-        probs[0,unk_idx]=0
-        probs[0,pad_idx]=0
+        probs[0, unk_idx] = 0
+        probs[0, pad_idx] = 0
 
-        probs_k, idx_k = torch.topk(probs, k=top_k)
-        next_id = torch.multinomial(probs_k, num_samples=1).item()
-        
-        word = idx2word[next_id]
-        
-        if word in ['<unk>', ' <pad> ']:
-            continue
-        
+        # Top-k sampling
+        top_probs, top_idx = torch.topk(probs, top_k)
+        top_probs = top_probs / top_probs.sum()
+        next_id = top_idx[0, torch.multinomial(top_probs, 1)].item()
+
         token.append(next_id)
+        context = context[1:] + [next_id]  # update context
 
     return " ".join(idx2word.get(i, "<unk>") for i in token)
 
